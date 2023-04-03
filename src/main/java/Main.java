de53eaa1215b744c2765daf9077a734a3c34d976
Ilya Kizilbashev <ikizil018@gmail.com>
2023-04-03 19:36:49 +0300

    test commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hallo world");
+
+        People human1 = new People(18, "Max");
+        People human2 = new People(20, "Alex");
+        System.out.println(human2);
+        System.out.println(human1);
+    }
+}
